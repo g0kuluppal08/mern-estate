d8f59a9d6d226cf8596a1a0259aef579282aa6be
g0kuluppal08 <97520505+g0kuluppal08@users.noreply.github.com>
2024-02-03 02:29:38 +0530

    pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import SignUp from './pages/SignUp';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import SignIn from './pages/Signin';
 
 function App() {
   return (
-   <h1 className='text-red-500'>App</h1>
+         <BrowserRouter>
+         <Routes>
+
+          <Route path='/' element={<Home/>}/>
+           <Route path='/sign-in' element={<SignIn/>}/>
+            <Route path='/sign-up' element={<SignUp/>}/>
+             <Route path='/about' element={<About/>}/>
+              <Route path='/profile' element={<Profile/>}/>
+          
+         </Routes>
+         </BrowserRouter>
+
   )
 }
 
