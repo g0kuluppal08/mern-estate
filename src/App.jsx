e21ae66e0de18b542b0dc974a5bc224a39594600
g0kuluppal08 <97520505+g0kuluppal08@users.noreply.github.com>
2024-02-14 10:33:04 +0530

    complete profile page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import About from './pages/About';
 import Profile from './pages/Profile';
 import SignIn from './pages/Signin';
 import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
@@ -17,7 +18,9 @@ function App() {
            <Route path='/sign-in' element={<SignIn/>}/>
             <Route path='/sign-up' element={<SignUp/>}/>
              <Route path='/about' element={<About/>}/>
+             <Route element={<PrivateRoute/>}>
               <Route path='/profile' element={<Profile/>}/>
+              </Route>
           
          </Routes>
          </BrowserRouter>
